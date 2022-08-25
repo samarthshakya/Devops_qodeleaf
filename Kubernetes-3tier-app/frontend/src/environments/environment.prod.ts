98b29e634a2c433f4adc4fff28035d67b12299c9
Samarth Shakya <samarthshakyatnp@gmail.com>
2022-08-26 03:21:08 +0530

    removing extras

--- Kubernetes-3tier-app/frontend/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  // TODO: update this
-  apiURL: 'http://a2e8d5688a0594658bfefb701419163f-239422344.us-west-2.elb.amazonaws.com/api/v1/hour-image'
-};
